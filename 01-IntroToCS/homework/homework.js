ae51ae2dc6ad97dad427945429bbffae34cbd603
stefano19911 <stefagiantorno@gmail.com>
2023-03-15 20:43:36 -0300

    tarea finalizada introtoCs

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,34 @@
 'use strict';
+1234
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var acc=0;
+   var posicion=num.length - 1;
+   for(var i=0;i<num.length;i++){
+      acc=acc + num[i] * Math.pow(2,posicion);
+      posicion --
 
-function DecimalABinario(num) {}
+   }
+return acc;
+
+}
+
+function DecimalABinario(num) {
+   var binario=[];
+   do{
+      if(num % 2 === 1){
+         binario.push(1);
+      }else{
+         binario.push(0);
+      } 
+      num = Math.floor(num / 2);
+
+   }while (num >= 1)
+      return binario.reverse().join("");
+
+ 
+
+}
 
 module.exports = {
    BinarioADecimal,
